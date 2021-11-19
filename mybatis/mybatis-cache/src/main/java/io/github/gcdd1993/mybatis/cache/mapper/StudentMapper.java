f18d3c8829320ac03eed65eca9e2ecd964d89c28
gcdd1993 <gcwm99@gmail.com>
2021-11-19 15:38:03 +0800

    修改配置路径为 /redisson.yaml即可读取classpath下的配置文件

--- mybatis/mybatis-cache/src/main/java/io/github/gcdd1993/mybatis/cache/mapper/StudentMapper.java
+++ mybatis/mybatis-cache/src/main/java/io/github/gcdd1993/mybatis/cache/mapper/StudentMapper.java
@@ -5,13 +5,24 @@ import io.github.gcdd1993.mybatis.cache.model.StudentPo;
 import org.apache.ibatis.annotations.CacheNamespace;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Property;
+import org.redisson.mybatis.RedissonCache;
 
 import java.io.Serializable;
 
 /**
  * Created By gcdd1993 On 2021/11/18.
  */
-@CacheNamespace
+@CacheNamespace(
+        implementation = RedissonCache.class,
+        eviction = RedissonCache.class,
+        properties = {
+                @Property(name = "timeToLive", value = "200000"),
+                @Property(name = "maxIdleTime", value = "100000"),
+                @Property(name = "maxSize", value = "100000"),
+                @Property(name = "redissonConfig", value = "/redisson.yaml")
+        }
+)
 @Mapper
 public interface StudentMapper extends BaseMapper<StudentPo> {
     @Override
