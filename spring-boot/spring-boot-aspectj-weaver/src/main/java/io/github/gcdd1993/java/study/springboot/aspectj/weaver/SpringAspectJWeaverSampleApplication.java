bb59781577ebdd5c26c4757545fe60c887e0ae39
gcdd1993 <gcwm99@gmail.com>
2021-11-22 17:03:51 +0800

    add spring gradle aspectj config sample

--- spring-boot/spring-boot-aspectj-weaver/src/main/java/io/github/gcdd1993/java/study/springboot/aspectj/weaver/SpringAspectJWeaverSampleApplication.java
+++ spring-boot/spring-boot-aspectj-weaver/src/main/java/io/github/gcdd1993/java/study/springboot/aspectj/weaver/SpringAspectJWeaverSampleApplication.java
@@ -4,13 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.AdviceMode;
 
 /**
  * SpringAop 使用 AspectJ Weaver实现内部调用代理
  * <p>
  * Created By gcdd1993 On 2021/11/22.
  */
-@EnableCaching
+@EnableCaching(mode = AdviceMode.ASPECTJ)
 @Slf4j
 @SpringBootApplication
 public class SpringAspectJWeaverSampleApplication {
