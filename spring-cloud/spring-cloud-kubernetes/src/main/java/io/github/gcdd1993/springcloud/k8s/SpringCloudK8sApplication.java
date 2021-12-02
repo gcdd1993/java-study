7996a1111038ae45d09096253cfe1f55999f16c5
gcdd1993 <gcwm99@gmail.com>
2021-12-02 20:19:18 +0800

    初始化Spring Cloud Kubernetes示例项目

---
+++ spring-cloud/spring-cloud-kubernetes/src/main/java/io/github/gcdd1993/springcloud/k8s/SpringCloudK8sApplication.java
@@ -0,0 +1,17 @@
+package io.github.gcdd1993.springcloud.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gcdd1993
+ * @date 2021/12/2
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class SpringCloudK8sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudK8sApplication.class, args);
+    }
+}
