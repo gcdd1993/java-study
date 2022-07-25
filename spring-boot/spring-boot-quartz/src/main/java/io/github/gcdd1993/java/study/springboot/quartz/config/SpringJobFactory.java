89d5ccb4afa7db1977ec15d4771e8e3439a369bf
Musk.Chen <Musk.Chen@fanruan.com>
2022-07-25 14:07:25 +0800

    fix: SpringBoot2默认支持Job注入Bean

--- spring-boot/spring-boot-quartz/src/main/java/io/github/gcdd1993/java/study/springboot/quartz/config/SpringJobFactory.java
+++ spring-boot/spring-boot-quartz/src/main/java/io/github/gcdd1993/java/study/springboot/quartz/config/SpringJobFactory.java
@@ -1,31 +1,33 @@
-package io.github.gcdd1993.java.study.springboot.quartz.config;
-
-import org.quartz.spi.TriggerFiredBundle;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
-import org.springframework.scheduling.quartz.AdaptableJobFactory;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
-
-/**
- * @author <a href="mailto:Musk.Chen@fanruan.com">Musk.Chen</a>
- * @version 2.1
- * Created by Musk.Chen on 2022/7/25
- */
-@Component
-public class SpringJobFactory extends AdaptableJobFactory {
-
-    @Resource
-    private AutowireCapableBeanFactory capableBeanFactory;
-
-    @Override
-    protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
-        //创建job实例
-        Object jobInstance = super.createJobInstance(bundle);
-        //进行注入
-        capableBeanFactory.autowireBean(jobInstance);
-
-        return jobInstance;
-    }
-
-}
+//package io.github.gcdd1993.java.study.springboot.quartz.config;
+//
+//import org.quartz.spi.TriggerFiredBundle;
+//import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+//import org.springframework.scheduling.quartz.AdaptableJobFactory;
+//import org.springframework.stereotype.Component;
+//
+//import javax.annotation.Resource;
+//
+///**
+// * SpringBoot 2 默认支持Job注入
+// *
+// * @author <a href="mailto:Musk.Chen@fanruan.com">Musk.Chen</a>
+// * @version 2.1
+// * Created by Musk.Chen on 2022/7/25
+// */
+//@Component
+//public class SpringJobFactory extends AdaptableJobFactory {
+//
+//    @Resource
+//    private AutowireCapableBeanFactory capableBeanFactory;
+//
+//    @Override
+//    protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
+//        //创建job实例
+//        Object jobInstance = super.createJobInstance(bundle);
+//        //进行注入
+//        capableBeanFactory.autowireBean(jobInstance);
+//
+//        return jobInstance;
+//    }
+//
+//}
